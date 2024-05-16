bb4939b691fcff66cd0ec6fe20b2ae2872d9ab5e
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-05-16 19:05:10 -0300

    comportamento do styleUrls, com multiplos files

--- secao3-fundamentos-do-angular-componentes-e-estilizacoes/meus-exemplos/projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
+++ secao3-fundamentos-do-angular-componentes-e-estilizacoes/meus-exemplos/projeto-components/src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,7 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrl: './card-button-cancel.component.scss',
+  // styleUrl: './card-button-cancel.component.scss',
+
+  /*E possível utilizar mais de um file de style, sendo o
+    2° - com preferência default, mas se caso no 1° a declaração do css
+    seja mais espeficia terá a preferência.
+    ex:
+        2° - .card-cancel-button {background-color: black;}
+        1° - div.card-cancel-button {background-color: black;} especifica que a class e de um div
+
+    CURIOSIDADE: e possível referência styles de outro component nesse, mas não
+    faz muito sentido. recomendação utilizar 1 arquivo de style somente.
+
+  */
+  styleUrls: [
+    './card-button-cancel.component.scss',
+    './card-button-cancel.component-2.scss',
+  ],
   /*
     E ao utiliza style externo(styleUrl) + interno(styles) caso
     aplique um style diferente será feito a mesclagem dos stilos
@@ -13,6 +29,7 @@ import { Component } from '@angular/core';
   */
 
   //Style local têm preferência em relação ao externo definido em styleUrl.
+  /*
   styles: [
     `
       .card-cancel-button {
@@ -28,13 +45,13 @@ import { Component } from '@angular/core';
     `,
 
     `
-    .card-cancel-button {
-      //O último style aplicado é o que prevale, caso seja aplicado
-      //em cima de uma mesma propriedade.
-      background-color: black;
-    }
-  `
-
+      .card-cancel-button {
+        //O último style aplicado é o que prevale, caso seja aplicado
+        //em cima de uma mesma propriedade.
+        background-color: black;
+      }
+    `,
   ],
+  */
 })
 export class CardButtonCancelComponent {}

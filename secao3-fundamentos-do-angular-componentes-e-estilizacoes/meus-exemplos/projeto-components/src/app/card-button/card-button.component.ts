b96d4d368e0e7ddd884bfa9f76088d9e2dddb144
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-05-16 18:12:28 -0300

    criando template html dentro do component --inline-template

--- secao3-fundamentos-do-angular-componentes-e-estilizacoes/meus-exemplos/projeto-components/src/app/card-button/card-button.component.ts
+++ secao3-fundamentos-do-angular-componentes-e-estilizacoes/meus-exemplos/projeto-components/src/app/card-button/card-button.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  templateUrl: './card-button.component.html',
-  styleUrl: './card-button.component.scss'
+  // templateUrl: './card-button.component.html',
+  template: ` <div class="card-button">TESTE</div> `,
+  styleUrl: './card-button.component.scss',
 })
-export class CardButtonComponent {
+export class CardButtonComponent {}
+/*
+  Template dentro do Componente (porp "template")
+  ex: (Crase)template: `
 
-}
+  `, -> mais de um linha de código, com quebra.
+
+      (aspas simples)template: '', -> para apenas uma linha, sem quebra.
+*/
